747d6835c0630c7572d97c2036ff0a3487a7096b
n0sec1 <98139875+n0sec1@users.noreply.github.com>
2022-01-21 13:33:48 -0500

    Experimenting with blogs

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,7 +2,7 @@ const projectsData = [
   {
     title: 'Giftcard Phishing Moitoring',
     description: `n0sec is working on a tool to automatically monitor and gather data on giftcard phishing ads conducted via Google Ads. This is investigated by downloading data and constantly checking the uptime of phishing ads.`,
-    imgSrc: '/static/images/vanilla.png',
+    imgSrc: '/static/images/vanilla.jpg',
     href: 'https://www.github.com',
   },
 ]
