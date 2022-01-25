12ed0ea9c4888a15fc937b99527ad945da483704
n0sec1 <98139875+n0sec1@users.noreply.github.com>
2022-01-25 07:22:49 -0500

    giscus test

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -33,10 +33,10 @@ const siteMetadata = {
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      repo: process.env.n0sec1/n0sec-blog,
+      repositoryId: process.env.R_kgDOGtt9HQ,
+      category: process.env.Announcements,
+      categoryId: process.env.DIC_kwDOGtt9Hc4CA2Q2,
       mapping: 'pathname', // supported options: pathname, url, title
       reactions: '0', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
